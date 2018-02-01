68b046095ebff2f1fc9d182d883a4903dff031ab
aedobbyn <adobbs11@gmail.com>
2018-02-01 11:33:58 -0800

    overwrite default installed packages with mine

--- R/01_write-installed-packages.R
+++ R/01_write-installed-packages.R
@@ -1,6 +1,9 @@
 ## deja vu from yesterday!
+library(tidyverse)
+library(here)
 
 ## create a data frame of your installed packages
+inst_packages <- installed.packages() %>% as_tibble()
 
 ## keep the variables
 ##   * Package
@@ -9,10 +12,14 @@
 ##   * Priority
 ##   * Built
 
+inst_packages <- inst_packages %>%
+  select(Package, LibPath, Version, Priority, Built)
+
 ## write it to data/installed-packages.csv
 ## YES overwrite the file that is there now
 ## that came from me (Jenny)
 ## it an example of what yours should look like
+
 
 ## when this script works, stage & commit it and the csv file
 ## PUSH!
